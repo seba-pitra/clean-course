0f6ad1da00c6702446ebe02af55c2eea5f2a7e71
Seba <sebastian.pitra10@gmail.com>
2023-02-24 11:43:46 -0300

    feat: code-smells folder created

---
+++ src/code-smells/02-high-coupling.ts
@@ -0,0 +1,59 @@
+(() => {
+  // No aplicando el principio de responsabilidad única
+  type Gender = "M" | "F";
+
+  // Alto Acoplamiento
+  // Se relacionan mucho entre si las clases y dificultan mucho los cambios y mantenimiento
+
+  class Person {
+    constructor(
+      public name: string,
+      public gender: Gender,
+      public birthdate: Date
+    ) {}
+  }
+
+  class User extends Person {
+    constructor(
+      public email: string,
+      public role: string,
+      private lastAccess: Date,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastFolderOpen: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, role, new Date(), name, gender, birthdate);
+    }
+  }
+
+  const userSettings = new UserSettings(
+    "/urs/home",
+    "/development",
+    "fernando@google.com",
+    "F",
+    "Fernando",
+    "M",
+    new Date("1985-10-21")
+  );
+
+  console.log({ userSettings, credentials: userSettings.checkCredentials() });
+})();
